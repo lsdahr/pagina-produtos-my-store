fa868336c6a78954794ec445f1016db3b85f44f5
lsdahr <luishora27@yahoo.com.br>
2021-10-01 20:15:09 -0300

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Impressora 3D',
+    price: 5999,
+    description: 'impressora feita para produtos 3D'
+  },
+  {
+    id: 2,
+    name: 'Robô tamanho real',
+    price: 2700,
+    description: 'robô tamanho real de humano'
+  },
+  {
+    id: 3,
+    name: 'Drone',
+    price: 4000,
+    description: 'drone para filmagens externas'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
